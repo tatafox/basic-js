7f42a3c83c86ea0e56462aca998da38144edf83a
Ваше имя@ <you@example.com>
2020-09-30 19:12:14 +0300

    fest: add extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
+module.exports = function repeater(str, options) {
+    
+  let resultStr = '';
+    
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }  
+  if (options.separator === undefined) { 
+    options.separator = '+';
+  }  
+  if (options.addition === undefined) { 
+    options.addition = '';
+  }  
+  if (options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 1;
+  }
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator == '';
+  }  
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+   
+    resultStr += String(str)
+    
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      j < options.additionRepeatTimes-1 
+      ? resultStr += String(options.addition) + String(options.additionSeparator)
+      : resultStr += String(options.addition);
+      
+    }
+
+    if (i < options.repeatTimes-1) { 
+      resultStr +=  String(options.separator);
+    } 
+  }
+
+  return resultStr;
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
   
