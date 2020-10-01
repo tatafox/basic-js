4cbceeddebcd771814d657e7f448cddac8e0da00
Ваше имя@ <you@example.com>
2020-10-01 14:01:51 +0300

    fest: add carbom dating & vigenere cipher solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,15 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  
+  if (typeof(sampleActivity) !== 'string'
+      || parseFloat(sampleActivity) <= 0
+      || isNaN(parseFloat(sampleActivity))
+      || parseFloat(sampleActivity) > MODERN_ACTIVITY) {
+  return false;
+  }
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) * HALF_LIFE_PERIOD / 0.693)
+
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,79 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  
+  constructor(reverse = true) {
+    this.reverse = reverse,
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  }
+
+
+
+  
+   encrypt(message, key) {
+    if (message == undefined || key == undefined) {
+      throw new Error();
+    }
+
+    let alph = this.alphabet;
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '',
+        j = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (alph.indexOf(message[i].toUpperCase()) > -1) {
+        let keyLetterNumber = j % key.length;
+        let letterNumber = (alph.indexOf(message[i]) + alph.indexOf(key[keyLetterNumber])) % 26;
+        result += alph[letterNumber];
+        j++;
+      } else {
+        result += message[i];
+      }
+    }
+
+    if (!this.reverse) {
+      return result.split('').reverse().join('');
+    } else {
+      return result;
+    }
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  
+
+
+
+
+  decrypt(message, key) {
+    if (message == undefined || key == undefined) {
+      throw new Error();
+    }
+
+    let alph = this.alphabet;
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '',
+        j = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].match(/^[A-Z]$/)) {
+        let keyLetterNumber = j % key.length;
+        let letterNumber = (alph.indexOf(message[i]) + 26 - this.alphabet.indexOf(key[keyLetterNumber])) % 26;
+        result += this.alphabet[letterNumber];
+        j++;
+      } else {
+        result += message[i];
+      }
+    }
+
+    if (!this.reverse) {
+      return result.split('').reverse().join('');
+    } else {
+      return result;
+    }
   }
 }
 
